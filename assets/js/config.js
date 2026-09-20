074aef9241418c7de0048e6dbf24eb424b25c294
contactrakpk <contact.rak.pk@gmail.com>
2026-09-20 17:17:00 +0500

    fixing errors detail

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,1 +1,9 @@
-window.RAK_API_URL = window.RAK_API_URL || 'http://localhost:8787';
+const isLocalDevelopment =
+  window.location.hostname === 'localhost' ||
+  window.location.hostname === '127.0.0.1';
+
+window.RAK_API_URL = window.RAK_API_URL || (
+  isLocalDevelopment
+    ? 'http://localhost:8787'
+    : ''
+);
